775a8d56b6c37bf493a91ba94039f3e1341056cc
Jayakodehode <jaya.kodehode@gmail.com>
2023-07-17 14:43:19 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Header } from "./components/header/header";
+import { Output } from "./components/output/output";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Header />
+        <Output />
+      </div>
+    </>
+  );
+}
+
+export default App;
