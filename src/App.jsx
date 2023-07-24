3b0beebb987a4c46effe056c716bc3927350371e
Jayakodehode <jaya.kodehode@gmail.com>
2023-07-24 14:10:19 +0200

    did changes in the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import { Header } from "./components/header/header";
 import { Output } from "./components/output/output";
-
 import "./App.css";
 
 function App() {
   return (
     <>
       <div className="container">
-        <Header />
-        <Output />
+        <div className="calculater-container">
+          <div className="header">
+            <h1>Calc</h1>
+            <div className="color-theme">
+              <h4>THEME</h4>
+              <div className="toogle-container">
+                <div className="toogle-numbers">
+                  <span>1</span>
+                  <span>2</span>
+                  <span>3</span>
+                </div>
+
+                <div class="theme-change">
+                  <div class="btn theme-1 active">1</div>
+                  <div class="btn theme-2">2</div>
+                  <div class="btn theme-3">3</div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Output />
+        </div>
       </div>
     </>
   );
