55e191bd73b3a8902acfbaa0930212454c753446
Jayakodehode <jaya.kodehode@gmail.com>
2023-07-25 14:23:11 +0200

    removed git from repository name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Calculater-React-Project.git",
+  base: "/Calculater-React-Project",
   plugins: [react()],
 });
